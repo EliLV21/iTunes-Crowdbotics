406f385d405e74c3ffc913492dfae3efca6219a2
Miriam Elizabeth Laguna Velazquez <Miriam Elizabeth Laguna Velazquez>
2017-08-24 13:07:19 -0500

    termino del servidor y gulpfile.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express         = require('express'); //Framework Express
+
+const app = express();
+
+app.use('/static', express.static(__dirname+'/node_modules'));
+app.use('/static', express.static(__dirname+'/src'));
+
+app.get('/',(req,res) =>{
+	res.sendFile(__dirname+'index.html');
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Server running on port '+port+'!');
+});
