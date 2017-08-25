91b04b1573408d3fd2e1e9fb3cfbf8309074df46
Miriam Elizabeth Laguna Velazquez <Miriam Elizabeth Laguna Velazquez>
2017-08-24 20:20:03 -0500

    no carga la imagen pero la maquetacion esta lista

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,6 @@
 var container = $("#containerSongs");
 
 function cargarPagina(){
-
 	var api = {
 	  url: 'https://itunes.apple.com/search?term=Taylor+Swift&entity=musicTrack&limit=200',
 	};
@@ -14,19 +13,19 @@ function cargarPagina(){
 	});
 	var list = function (tracks){
 		var containerTracks = "";
+		var contador = 0;
 		var templateTracks =
-			'<ul class="collection">' +
+			'<ul class="collection" data="__data__">' +
 				'<li class="collection-item avatar">' +
 					'<img src="__image__" alt="" class="circle">' +
-					'<span class="title">Tyler Swift</span>' +
-					'<p>Track name: __track__</p>' +
-					'<p>Collection name: __collection__</p>' +
-					'<p>Track Id: __id__</p>' +
-					'<a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>' +
+					'<p><strong>__track__</strong></p>' +
+					'<p>__collection__</p>' +
+					'<p class="secondary-content">__id__</p>' +
 				'</li>' +
 			'</ul>';
 		tracks.forEach(function(track, indice){
-			containerTracks += templateTracks.replace('__image__', tracks[indice].collectionViewUrl)
+			containerTracks += templateTracks.replace('__data__', "data-url".contador++)
+								.replace('__image__', tracks[indice].artworkUrl130)
 								.replace('__track__', tracks[indice].trackName)
 								.replace('__collection__', tracks[indice].collectionName)
 								.replace('__id__', tracks[indice].trackId)
